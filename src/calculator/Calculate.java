adaef9ca7ac36f6079770a676e3f5a2ff1cd7cf0
BrightStarIsNameBetelgeuse <mainyvar@gmail.com>
2014-09-25 08:21:39 +0600

    create project

---
+++ src/calculator/Calculate.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package calculator;
+
+/**
+ *
+ * @author rodki143
+ */
+public class Calculate {
+
+    private static Calculate instance;
+
+    public Calculate getInstance(){
+        if(inst)
+    }
+}
